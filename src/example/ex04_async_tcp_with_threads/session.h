477ae6cabea1508196169e4079e9b78371e11357
Ryan <b04502132@ntu.edu.tw>
2022-08-09 09:59:37 +0800

    add ex04 client.

--- src/example/ex04_async_tcp_with_threads/session.h
+++ src/example/ex04_async_tcp_with_threads/session.h
@@ -17,7 +17,7 @@ public:
 
     void start()
     {
-        writeMsg();
+        // readMsg(10);
         boost::asio::async_read_until(socket, 
                                       streambuf, 
                                       '\n', 
@@ -25,6 +25,7 @@ public:
         {
             std::cout << std::istream(&self->streambuf).rdbuf();
         });
+        writeMsg();
     }
 
     void writeMsg()
@@ -38,10 +39,41 @@ public:
             });
     }
 
+    void writeMsg1()
+    {
+        for(auto i = 0 ; i < received_data.size() ; i++)
+            std::cout << received_data[i] << " ";
+        boost::asio::async_write(
+            socket,
+            boost::asio::buffer(received_data, 10),
+            [self = shared_from_this()](boost::system::error_code error, std::size_t bytes_transferred)
+            {
+                std::cout << ">>> send message completed" << std::endl;
+            });
+    }
+
+    void readMsg(uint8_t data_size)
+    {
+        auto self(shared_from_this());
+        boost::asio::async_read(
+            socket,
+            boost::asio::buffer(received_data, data_size),
+            [this, self](boost::system::error_code ec, std::size_t length)
+            {
+                if (!ec)
+                {
+                    // send back to client
+                    writeMsg1();
+                }
+            }
+        );
+        
+    }
     
 
 private:
 
     boost::asio::ip::tcp::socket socket;
     boost::asio::streambuf streambuf;
+    std::vector<char> received_data = std::vector<char>(10);
 };

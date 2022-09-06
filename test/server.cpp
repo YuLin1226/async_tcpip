f983a4aa3370fb82088c4f5d86b454fc0d6e074d
Ryan <b04502132@ntu.edu.tw>
2022-09-06 11:42:11 +0800

    add sample contents

--- test/server.cpp
+++ test/server.cpp
@@ -2,14 +2,31 @@
 
 namespace TCP
 {
-    Server::Server()
+    Server::Server(boost::asio::io_context& io_context)
+    : io_context_(io_context)
+    , platform_acceptor_(io_context_, 
+                        boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), PLATFORM_PORT))
     {
-
+        std::cout << ">>> Server created." << std::endl;
+        accept();
     }
     
     Server::~Server()
     {
+        std::cout << ">>> Server destroyed." << std::endl;
+    }
 
+    void Server::platform_accept()
+    {
+        platform_socket_ptr_ = std::make_shared<boost::asio::ip::tcp::socket>(io_context_);
+        auto timer_ptr = std::make_shared<std::shared_ptr<boost::asio::deadline_timer>>(io_context_);
+        platform_acceptor_.async_accept(
+            *platform_socket_ptr_, 
+            [&](boost::system::error_code error)
+            {
+                // std::make_shared<SessionPlatform>(std::move(*platform_socket_ptr_), std::move(*timer_ptr));
+                platform_accept();
+            });
     }
 
 } // namespace TCP
--- test/server.hpp
+++ test/server.hpp
@@ -10,9 +10,20 @@ namespace TCP
     {
     private:
         
+        const unsigned int PLATFORM_PORT = 8888;
+
+        boost::asio::io_context& io_context_;
+        boost::asio::ip::tcp::acceptor platform_acceptor_;
+        std::shared_ptr<boost::asio::ip::tcp::socket> platform_socket_ptr_;
+
+
     public:
-        Server();
+        Server(boost::asio::io_context& io_context);
         ~Server();
+
+        void accept();
+        void platform_accept();
+
     };
     
     

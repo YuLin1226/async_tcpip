890b5b6db64cec2b809eb654b11781b63d52c764
Ryan <b04502132@ntu.edu.tw>
2022-09-07 09:42:01 +0800

    add platform session template

--- Npairs/server/session/session_platform.hpp
+++ Npairs/server/session/session_platform.hpp
@@ -1,25 +1,63 @@
-#ifndef _SESSION_PLATFORM_HPP_
+#if !defined(_SESSION_PLATFORM_HPP_)
 #define _SESSION_PLATFORM_HPP_
 
-#include "session.hpp"
+#include <memory>
+#include <functional>
+#include <boost/asio.hpp>
+#include "frame/frame_platform.hpp"
 
-class SessionPlatform : public Session
+
+namespace Session
 {
-private:
-    
 
+class SessionPlatform : public std::enable_shared_from_this<SessionPlatform>
+{
+    public:
+        SessionPlatform(boost::asio::io_context& ioContext, boost::asio::ip::tcp::socket &&socket);
+        ~SessionPlatform();
 
-public:
-    SessionPlatform(std::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr, std::shared_ptr<boost::asio::deadline_timer> timer_ptr);
+        enum class ConnectionStatus {CONNECTED, STOPPING, STOPPED};
 
-    ~SessionPlatform();
+        using OnReadCallback = std::function<void(const Frame::FramePlatform&)>;
+        using OnBrokenCallback = std::function<void()>;
+        using OnDisConnectedCallback = std::function<void()>;
+        
+        void start(OnReadCallback onReadCallback, OnBrokenCallback onBrokenCallback);
 
-    void writeMessage() override;
+        void stop(OnDisConnectedCallback callback);
 
-    void readMessage() override;
+        void write(unsigned char *pStart, size_t dataSize);
+
+    private:
+        static const int PREPARE_READ_BUFFER_SIZE = 1024;
+
+        ConnectionStatus connectStatus_;
+        boost::asio::ip::tcp::socket socket_;
+        boost::asio::io_context::strand writeStrand_, readStrand_;
+        boost::asio::streambuf incoming_;
+        std::mutex mtxOutgoingQueue_;
+        boost::asio::streambuf outgoing_, outgoingQueue_;
+        int isAsyncWriteCompleteHandlerCalled_, isAsyncReadCompleteHandlerCalled_;
+
+        OnReadCallback onReadCallback_;
+        OnDisConnectedCallback  onDisConnectedCallback_;
+        OnBrokenCallback onBrokenCallback_;
+
+        //should be run in io_contex thread
+        void issueDisConnect();
+
+        //should be called in io_context thread
+        void read();
+        void onRead(boost::system::error_code error, std::size_t bytesTransferred);
+
+        //should be called in io context thread
+        void moveFromQueueAndWrite();
+        void onWrite(boost::system::error_code error, std::size_t bytesTransferred);
+
 
 };
 
 
+} // namespace Session
 
-#endif
+#endif // _SESSION_PLATFORM_HPP_
